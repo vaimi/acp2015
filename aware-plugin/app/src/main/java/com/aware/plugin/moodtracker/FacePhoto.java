eeba5ee8e17d7758d11a56265db707fbf00e5809
Mikko Väisänen <mjvaisanen@gmail.com>
2016-01-10 14:52:21 +0200

    Started work on camera

---
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/FacePhoto.java
@@ -0,0 +1,84 @@
+package com.aware.plugin.moodtracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class FacePhoto extends Service {
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    private Camera.Parameters cameraParameters;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(Plugin.TAG, "Created camera service");
+
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        int cameraId = getFrontCameraId();
+        if (cameraId != -1) {
+            camera = Camera.open(cameraId);
+            SurfaceView surfaceView = new SurfaceView(getApplicationContext());
+            try {
+                camera.setPreviewDisplay(surfaceView.getHolder());
+                cameraParameters = camera.getParameters();
+                camera.setParameters(cameraParameters);
+                camera.startPreview();
+                camera.takePicture(null, null, pictureCallback);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.d(Plugin.TAG, "No front facing camera");
+        }
+    }
+
+    @Override
+    public void onStart(Intent intent, int startID) {
+        super.onStart(intent, startID);
+    }
+
+    /*@Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }*/
+
+    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(final byte[] data, Camera camera) {
+            Log.d(Plugin.TAG, "Picture taken");
+        }
+    };
+
+    int getFrontCameraId() {
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        for (int i=0; i<Camera.getNumberOfCameras(); i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) return i;
+        }
+        return -1;
+    }
+}
