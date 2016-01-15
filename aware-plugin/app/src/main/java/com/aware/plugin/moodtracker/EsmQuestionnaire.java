b0377a3bfde014a41b0ee96650824293b931bcce
zhanna <zhanna.sarsenbayeva@gmail.com>
2016-01-15 16:03:12 +0200

    Commit from Zhanna: Esm questionnaire

--- aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmQuestionnaire.java
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmQuestionnaire.java
@@ -1,7 +1,16 @@
 package com.aware.plugin.moodtracker;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by vaimi on 3.12.2015.
  */
-public class EsmQuestionnaire {
+public class EsmQuestionnaire extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esmscreen);
+    }
 }
