db2898d8c0bde1a8af3cc1f60a5457f8a9167102
zhanna <zhanna.sarsenbayeva@gmail.com>
2016-01-19 17:47:40 +0200

    Adding msiley images finally has worked

--- aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmQuestionnaire.java
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmQuestionnaire.java
@@ -2,15 +2,42 @@ package com.aware.plugin.moodtracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 /**
  * Created by vaimi on 3.12.2015.
  */
 public class EsmQuestionnaire extends Activity {
 
+    private SeekBar seekBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.esmscreen);
-    }
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekbar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        }
+
+
+        }
 }
