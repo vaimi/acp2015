35acaee3f07c67c5b000b0b8cd2162c7564b9835
Jilin Yang <yjlyangjilin@gmail.com>
2016-02-22 13:25:24 +0200

    Change contextcard to standalone activity, seems to be working

---
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/MoodHistory.java
@@ -0,0 +1,29 @@
+package com.aware.plugin.moodtracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.aware.Aware;
+
+/**
+ * Created by Jilin on 2/22/2016.
+ */
+public class MoodHistory extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moodhistory);
+
+        Intent aware = new Intent(this, Aware.class);
+        startService(aware);
+
+        LinearLayout parent = (LinearLayout) findViewById(R.id.activity_card);
+        parent.addView(new ContextCard().getContextCard(getApplicationContext()));
+    }
+}
