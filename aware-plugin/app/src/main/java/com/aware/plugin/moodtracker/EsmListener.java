d5451a865485237a6c738acb14887cf59f84a1f6
Mikko Väisänen <mjvaisanen@gmail.com>
2016-02-25 13:15:46 +0200

    Workaround for repeating EMSs. Not currently enabled.

--- aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmListener.java
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/EsmListener.java
@@ -3,9 +3,12 @@ package com.aware.plugin.moodtracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.util.Log;
 
 import com.aware.Aware;
+
+import java.util.TreeMap;
 
 /**
  * Created by Mikko on 18.2.2016.
@@ -19,6 +22,18 @@ public class EsmListener extends BroadcastReceiver {
         if (!Aware.getSetting(c, Settings.STATUS_PLUGIN_MOODTRACKER_ESM).equals("1")) {
             return;
         }
+
+        // Workaround to filer esm entries
+        /*Cursor cursor = context.getContentResolver()
+                .query(Provider.Moodtracker_Data.CONTENT_URI, new String[] {"timestamp"}, "trigger='ESMHAPPINESS'", null, "timestamp DESC LIMIT 1");
+        if (cursor != null && cursor.moveToFirst()) {
+            long now = System.currentTimeMillis();
+            if ((now - cursor.getLong(0)) < 3600000) {
+                if (Plugin.DEBUG) Log.d(Plugin.TAG, "ESM was shown < hour ago. Aborting");
+                return;
+            }
+        }*/
+
         Intent esmIntent = new Intent(context, EsmQuestionnaire.class);
         esmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(esmIntent);
