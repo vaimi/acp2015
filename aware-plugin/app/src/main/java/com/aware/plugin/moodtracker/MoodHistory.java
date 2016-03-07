203e6253a0561ea96e16153809e435d27fdb0717
Mikko Väisänen <mjvaisanen@gmail.com>
2016-03-07 21:12:39 +0200

    Fixed separate mood history crashing when the history is disabled

--- aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/MoodHistory.java
+++ aware-plugin/app/src/main/java/com/aware/plugin/moodtracker/MoodHistory.java
@@ -7,7 +7,9 @@ import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.aware.Aware;
 
@@ -19,14 +21,20 @@ public class MoodHistory extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_moodhistory);
-
         Intent aware = new Intent(this, Aware.class);
         startService(aware);
         Intent plugin = new Intent(this, Plugin.class);
         startService(plugin);
         SystemClock.sleep(500);
+        setContentView(R.layout.activity_moodhistory);
         LinearLayout parent = (LinearLayout) findViewById(R.id.activity_card);
-        parent.addView(new ContextCard().getContextCard(getApplicationContext()));
+        if (!Aware.getSetting(getApplicationContext(), Settings.STATUS_PLUGIN_MOODTRACKER_CONTEXTCARD).equals("1")) {
+            TextView notEnabled = new TextView(this);
+            notEnabled.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            notEnabled.setText("Not enabled");
+            parent.addView(notEnabled);
+        } else  {
+            parent.addView(new ContextCard().getContextCard(getApplicationContext()));
+        }
     }
 }
